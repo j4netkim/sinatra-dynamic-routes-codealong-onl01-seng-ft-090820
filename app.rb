cc41e44050f60449e4b056ee679458d8a31840a2
J K <jkim65@buffalo.edu>
2020-10-23 22:45:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,9 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    "/multiple/6/6"
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    @product = @num1 * @num2
   end
 
 end
